2cb5018d8ac3aba298c7e1ca03827424b4ba5e82
jameslee1187 <jameslee1187@gmail.com>
2018-10-14 17:36:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   
   attr_accessor :bank_account, :happiness, :hygiene
-  attr_reader :person
+  attr_reader :name
   
-  def initialize(person)
-    @person = person
+  def initialize(name)
+    @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
